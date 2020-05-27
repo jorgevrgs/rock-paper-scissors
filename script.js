b9c7f63c8edaa6ff6ccf31dae7b45c85fd009a2d
Jorge Vargas <jorgevargasla@gmail.com>
2020-05-27 12:34:37 -0500

    include counter, missing ending match

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const matchPlayer = document.getElementById('match-player');
 const matchComputer = document.getElementById('match-computer');
 const matchResult = document.getElementById('match-result');
 
+const scorePlayer = document.getElementById('score-player');
+const scoreComputer = document.getElementById('score-computer');
+let player = 0;
+let computer = 0;
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) { return 'equals'; }
 
@@ -13,9 +18,11 @@ function playRound(playerSelection, computerSelection) {
 
   if (typeof combinations[playerSelection] !== 'undefined'
   && combinations[playerSelection] === computerSelection) {
+    player += 1;
     return 'greater-than';
   } else if (typeof combinations[computerSelection] !== 'undefined'
   && combinations[computerSelection] === playerSelection) {
+    computer += 1;
     return 'less-than';
   }
 }
@@ -27,9 +34,9 @@ function computerPlay() {
 }
 
 function cleanMatch() {
-  matchPlayer.className = 'separator';
-  matchComputer.className = 'separator';
-  matchResult.className = 'separator';
+  matchPlayer.className = 'icons';
+  matchComputer.className = 'icons';
+  matchResult.className = 'icons';
 }
 
 function handleClick(e) {
@@ -44,6 +51,9 @@ function handleClick(e) {
   matchPlayer.innerHTML = `<i class="far fa-hand-${playerSelection}"></i>`;
   matchResult.innerHTML = `<i class="fas fa-${playResult}"></i>`;
   matchComputer.innerHTML = `<i class="far fa-hand-${computerSelection}"></i>`;
+
+  scorePlayer.innerHTML = `<i>${player}</i>`
+  scoreComputer.innerHTML = `<i>${computer}</i>`
 
 }
 
